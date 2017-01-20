b0324446adbf8d454987b14b9fcb8a324ec5f8bf
marbug <marbuga@gmail.com>
2017-01-20 20:29:10 +0200

    add scripts

---
+++ scripts/use-bitbucket.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@bitbucket.org:marbug/nodejs-and-mongodb.git
+
+git remote -v
---
+++ scripts/use-github.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@github.com:marbug/nodejs-and-mongodb.git
+
+git remote -v
